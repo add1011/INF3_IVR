1bf80a532e9c2960e1286d2514ce9acb0080e3d2
add1011 <43935446+add1011@users.noreply.github.com>
2020-11-08 15:42:29 +0000

    Add files via upload
    Added code to detect joints in both cameras, currently if a joint can't be found it uses the last known location.

--- ivr_assignment/src/image1.py
+++ ivr_assignment/src/image1.py
@@ -28,8 +28,93 @@ class image_converter:
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
     
-    self.time = rospy.get_time()
+    # save the position of the red and green joints to use them if they cannot be found
+    self.red_pos = np.array([0, 0])
+    self.green_pos = np.array([0, 0])
 
+  def detect_red(self,image):
+      redMask = cv2.inRange(self.img1HSV, (0, 100, 100), (10, 255, 255))
+      redImg = cv2.bitwise_and(self.cv_image1, self.cv_image1, mask=redMask)
+      redImg_grey = cv2.cvtColor(redImg, cv2.COLOR_BGR2GRAY)
+      ret, thresh = cv2.threshold(redImg_grey, 1, 255, 0)
+      M = cv2.moments(thresh)
+      if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        self.red_pos = [cX, cY]
+      else:
+        cX, cY = self.red_pos
+      
+      cv2.circle(self.cv_image1, (cX, cY), 2, (255, 255, 255), -1)
+      """
+      cv2.putText(self.cv_image1, "Red Center", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+      """
+      return np.array([cX, cY])
+
+  def detect_green(self,image):
+      greenMask = cv2.inRange(self.img1HSV, (50, 100, 100), (70, 255, 255))
+      greenImg = cv2.bitwise_and(self.cv_image1, self.cv_image1, mask=greenMask)
+      greenImg_grey = cv2.cvtColor(greenImg, cv2.COLOR_BGR2GRAY)
+      ret, thresh = cv2.threshold(greenImg_grey, 1, 255, 0)
+      M = cv2.moments(thresh)
+      if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        self.green_pos = [cX, cY]
+      else:
+        cX, cY = self.green_pos
+
+      cv2.circle(self.cv_image1, (cX, cY), 2, (255, 255, 255), -1)
+      """
+      cv2.putText(self.cv_image1, "Green Center", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+      """
+      return np.array([cX, cY])
+
+  def detect_blue(self,image):
+      blueMask = cv2.inRange(self.img1HSV, (110, 100, 100), (130, 255, 255))
+      blueImg = cv2.bitwise_and(self.cv_image1, self.cv_image1, mask=blueMask)
+      blueImg_grey = cv2.cvtColor(blueImg, cv2.COLOR_BGR2GRAY)
+      ret, thresh = cv2.threshold(blueImg_grey, 1, 255, 0)
+      M = cv2.moments(thresh)
+      if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+      else:
+        cX, cY = 0, 0
+      
+      cv2.circle(self.cv_image1, (cX, cY), 2, (255, 255, 255), -1)
+      """
+      cv2.putText(self.cv_image1, "Blue Center", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+      """
+      
+      return np.array([cX, cY])
+
+  def detect_yellow(self,image):
+      yellowMask = cv2.inRange(self.img1HSV, (20, 100, 100), (40, 255, 255))
+      yellowImg = cv2.bitwise_and(self.cv_image1, self.cv_image1, mask=yellowMask)
+      yellowImg_grey = cv2.cvtColor(yellowImg, cv2.COLOR_BGR2GRAY)
+      ret, thresh = cv2.threshold(yellowImg_grey, 1, 255, 0)
+      M = cv2.moments(thresh)
+      if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+      else:
+        cX, cY = 0, 0
+      
+      cv2.circle(self.cv_image1, (cX, cY), 2, (255, 255, 255), -1)
+      """
+      cv2.putText(self.cv_image1, "Yellow Center", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+      """
+      
+      return np.array([cX, cY])
+  
+  def pixel2meter(self,image):
+      # Obtain the centre of each coloured blob
+      circle1Pos = self.detect_blue(image)
+      circle2Pos = self.detect_green(image)
+      # find the distance between two circles
+      dist = np.sum((circle1Pos - circle2Pos)**2)
+      return 2.5 / np.sqrt(dist)
 
   # Recieve data from camera 1, process it, and publish
   def callback1(self,data):
@@ -38,6 +123,14 @@ class image_converter:
       self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
+    
+    
+    self.img1HSV = cv2.cvtColor(self.cv_image1, cv2.COLOR_BGR2HSV)
+
+    self.detect_red(self.cv_image1)
+    self.detect_green(self.cv_image1)
+    self.detect_blue(self.cv_image1)
+    self.detect_yellow(self.cv_image1)
     
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
@@ -49,20 +142,23 @@ class image_converter:
     
     t = rospy.get_time()
     
-    #
-    self.joint1 =Float64()
+    
+    self.joint1 = Float64()
     self.joint1.data = np.pi/2*np.sin((np.pi/15)*t)
-    self.joint2 =Float64()
+    self.joint2 = Float64()
     self.joint2.data = np.pi/2*np.sin((np.pi/18)*t)
-    self.joint3 =Float64()
+    self.joint3 = Float64()
     self.joint3.data = np.pi/2*np.sin((np.pi/20)*t)
+    
     
     # Publish the results
     try: 
       self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
+      """
       self.robot_joint1_pub.publish(self.joint1)
       self.robot_joint2_pub.publish(self.joint2)
       self.robot_joint3_pub.publish(self.joint3)
+      """
     except CvBridgeError as e:
       print(e)
 
--- ivr_assignment/src/image2.py
+++ ivr_assignment/src/image2.py
@@ -23,7 +23,87 @@ class image_converter:
     self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw",Image,self.callback2)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
+    
+    # save the position of the red and green joints to use them if they cannot be found
+    self.red_pos = np.array([0, 0])
+    self.green_pos = np.array([0, 0])
 
+  def detect_red(self,image):
+      redMask = cv2.inRange(self.img2HSV, (0, 100, 100), (10, 255, 255))
+      redImg = cv2.bitwise_and(self.cv_image2, self.cv_image2, mask=redMask)
+      redImg_grey = cv2.cvtColor(redImg, cv2.COLOR_BGR2GRAY)
+      ret, thresh = cv2.threshold(redImg_grey, 1, 255, 0)
+      M = cv2.moments(thresh)
+      if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        self.red_pos = [cX, cY]
+      else:
+        cX, cY = self.red_pos
+      
+      cv2.circle(self.cv_image2, (cX, cY), 2, (255, 255, 255), -1)
+      """
+      cv2.putText(self.cv_image2, "Red Center", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+      """
+      return np.array([cX, cY])
+
+  def detect_green(self,image):
+      greenMask = cv2.inRange(self.img2HSV, (50, 100, 100), (70, 255, 255))
+      greenImg = cv2.bitwise_and(self.cv_image2, self.cv_image2, mask=greenMask)
+      greenImg_grey = cv2.cvtColor(greenImg, cv2.COLOR_BGR2GRAY)
+      ret, thresh = cv2.threshold(greenImg_grey, 1, 255, 0)
+      M = cv2.moments(thresh)
+      if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        self.green_pos = [cX, cY]
+      else:
+        cX, cY = self.green_pos
+      
+      cv2.circle(self.cv_image2, (cX, cY), 2, (255, 255, 255), -1)
+      """
+      cv2.putText(self.cv_image2, "Green Center", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+      """
+      
+      return np.array([cX, cY])
+
+  def detect_blue(self,image):
+      blueMask = cv2.inRange(self.img2HSV, (110, 100, 100), (130, 255, 255))
+      blueImg = cv2.bitwise_and(self.cv_image2, self.cv_image2, mask=blueMask)
+      blueImg_grey = cv2.cvtColor(blueImg, cv2.COLOR_BGR2GRAY)
+      ret, thresh = cv2.threshold(blueImg_grey, 1, 255, 0)
+      M = cv2.moments(thresh)
+      if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+      else:
+        cX, cY = 0, 0
+      
+      cv2.circle(self.cv_image2, (cX, cY), 2, (255, 255, 255), -1)
+      """
+      cv2.putText(self.cv_image2, "Blue Center", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+      """
+      
+      return np.array([cX, cY])
+
+  def detect_yellow(self,image):
+      yellowMask = cv2.inRange(self.img2HSV, (20, 100, 100), (40, 255, 255))
+      yellowImg = cv2.bitwise_and(self.cv_image2, self.cv_image2, mask=yellowMask)
+      yellowImg_grey = cv2.cvtColor(yellowImg, cv2.COLOR_BGR2GRAY)
+      ret, thresh = cv2.threshold(yellowImg_grey, 1, 255, 0)
+      M = cv2.moments(thresh)
+      if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+      else:
+        cX, cY = 0, 0
+      
+      cv2.circle(self.cv_image2, (cX, cY), 2, (255, 255, 255), -1)
+      """
+      cv2.putText(self.cv_image2, "Yellow Center", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+      """
+      
+      return np.array([cX, cY])
 
   # Recieve data, process it, and publish
   def callback2(self,data):
@@ -32,6 +112,14 @@ class image_converter:
       self.cv_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
+    
+    self.img2HSV = cv2.cvtColor(self.cv_image2, cv2.COLOR_BGR2HSV)
+
+    self.detect_red(self.cv_image2)
+    self.detect_green(self.cv_image2)
+    self.detect_blue(self.cv_image2)
+    self.detect_yellow(self.cv_image2)
+    
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
     im2=cv2.imshow('window2', self.cv_image2)
